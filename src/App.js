c914fe9a5533fba5f3e086d495680a3aecbe7d1e
Maneesh John <marajeys@gmail.com>
2023-04-13 16:04:39 -0500

    Added table demo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
+import EnhancedTable from './EnhancedTable';
 
 class App extends React.Component {
   constructor(props) {
@@ -10,20 +17,32 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {this.props.testVal}
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+
+        <div className="App-body">
+          <Box sx={{ minWidth: 120 }} marginBottom={5}>
+            <EnhancedTable />
+          </Box>
+
+          <Box sx={{ minWidth: 120 }}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Chart</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={10}
+                label="Age"
+              >
+                <MenuItem value={10}>Analytic 1</MenuItem>
+                <MenuItem value={20}>Analytic 2</MenuItem>
+                <MenuItem value={30}>Analytic 3</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+          
+        </div>
       </div>
+      
     );
   }
 }
