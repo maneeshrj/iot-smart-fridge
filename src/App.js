6e019b074bb06293521d716e24f3e8707e43b4cf
GokesT <48070460+GokesT@users.noreply.github.com>
2023-04-27 14:55:48 -0500

    worked on item expiry analytic

--- src/App.js
+++ src/App.js
@@ -146,95 +146,118 @@ class App extends React.Component {
       analytic: analyticName,
       loadingAnalytic: true,
     }));
-
-    get(child(this.props.userRef, analyticName)).then((snapshot) => {
-      if (snapshot.exists()) {
-        console.log('Analytic loaded:', snapshot.val());
+  
+    if (analyticName === 'expires_count') {
+      const itemsRef = ref(getDatabase(), this.props.userRef + '/items');
+      const expirationCounts = {};
+      onValue(itemsRef, (snapshot) => {
+        snapshot.forEach((itemSnapshot) => {
+          const item = itemSnapshot.val();
+          const expirationDate = item.expiry_date;
+          if (expirationDate in expirationCounts) {
+            expirationCounts[expirationDate] += 1;
+          } else {
+            expirationCounts[expirationDate] = 1;
+          }
+        });
+        const data = Object.entries(expirationCounts).map(([key, value]) => ({ name: key, count: value }));
+        console.log('Analytic loaded:', data);
         this.setState((prevState) => ({
           ...prevState,
-          analyticData: snapshot.val(),
+          analyticData: data,
           loadingAnalytic: false,
         }));
-      } else {
-        console.log("No analytic data available");
-      }
-    }).catch((error) => {
-      console.log('Error loading analytic: '+error);
-    });
+      }, {
+        onlyOnce: true
+      });
+    } else {
+      get(child(this.props.userRef, analyticName)).then((snapshot) => {
+        if (snapshot.exists()) {
+          console.log('Analytic loaded:', snapshot.val());
+          this.setState((prevState) => ({
+            ...prevState,
+            analyticData: snapshot.val(),
+            loadingAnalytic: false,
+          }));
+        } else {
+          console.log("No analytic data available");
+        }
+      }).catch((error) => {
+        console.log('Error loading analytic: '+error);
+      });
+    }
   };
+  
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
         </header>
+        <Paper 
+          className="App-body"
+          sx={{
+            m: 2,
+            display: 'flex',
+            flexDirection: 'column',
+            bgcolor: 'linen',
+          }}
+        >
+          <Box sx={{ minWidth: 120 }} mb={3}>
+            <EnhancedTable 
+              rows={this.state.rows}
+              addRow={this.addRow} 
+              deleteRows={this.deleteRows}
+              updateRows={this.updateRows}
+            />
+          </Box>
+  
           <Paper 
-            className="App-body"
-            sx={{
-              m: 2,
+            sx={{ 
+              minWidth: 120, 
+              p: 2,
               display: 'flex',
               flexDirection: 'column',
-              bgcolor: 'linen',
+              justifyContent: 'center',
+              alignItems: 'center'
             }}
           >
-            <Box sx={{ minWidth: 120 }} mb={3}>
-              <EnhancedTable 
-                rows={this.state.rows}
-                addRow={this.addRow} 
-                deleteRows={this.deleteRows}
-                updateRows={this.updateRows}
-              />
-            </Box>
-
-            <Paper 
-              sx={{ 
-                minWidth: 120, 
-                p: 2,
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center'
-              }}
-            >
-              <Typography variant='h6' mb={1} >Analytics</Typography>
-              <FormControl fullWidth>
-                <Select
-                  value={this.state.analytic}
-                  onChange={this.handleAnalyticSelect}
+            <Typography variant='h6' mb={1} >Analytics</Typography>
+            <FormControl fullWidth>
+              <Select
+                value={this.state.analytic}
+                onChange={this.handleAnalyticSelect}
+              >
+                <MenuItem value={'added_count'}>How often are items added?</MenuItem>
+                <MenuItem value={'expired_count'}>How often do items expire?</MenuItem>
+              </Select>
+            </FormControl>
+            {this.state.loadingAnalytic ? (
+              <Typography variant='h6' m={1} >Loading...</Typography>
+            ) : this.state.analyticData === 'empty' ? (
+              <Typography variant='h6' m={1} >No data available</Typography>
+            ) : (
+              <ResponsiveContainer width="80%" height={80*this.state.analyticData.length}>
+                <BarChart
+                  layout="vertical"
+                  data={this.state.analyticData}
+                  margin={{ top: 20, right: 20, bottom: 5, left: 0 }}
                 >
-                  <MenuItem value={'added_count'}>How often are items added?</MenuItem>
-                  <MenuItem value={'expired_count'}>How often do items expire?</MenuItem>
-                </Select>
-              </FormControl>
-              { this.state.loadingAnalytic ?
-                <Typography variant='h6' m={1} >Loading...</Typography>
-                :
-                (
-                  (this.state.analyticData === 'empty') ?
-                  <Typography variant='h6' m={1} >No data available</Typography>
-                  :
-                  <ResponsiveContainer width="80%" height={80*this.state.analyticData.length}>
-                    <BarChart
-                      layout="vertical"
-                      data={this.state.analyticData}
-                      margin={{ top: 20, right: 20, bottom: 5, left: 0 }}
-                    >
-                      <CartesianGrid stroke="#ccc" />
-                      <XAxis type="number" tickCount={10}/>
-                      <YAxis dataKey="name" type="category"/>
-                      <Legend formatter={(value, entry, index) => {return value.charAt(0).toUpperCase() + value.slice(1)}}/>
-                      <Bar dataKey="count" fill="#82ca9d" />
-                      <Tooltip />
-                    </BarChart>
-                  </ResponsiveContainer>
-                )
-              }
+                  <CartesianGrid stroke="#ccc" />
+                  <XAxis type="number" tickCount={10}/>
+                  <YAxis dataKey="name" type="category"/>
+                  <Legend formatter={(value, entry, index) => {return value.charAt(0).toUpperCase() + value.slice(1)}}/>
+                  <Bar dataKey="count" fill="#82ca9d" />
+                  <Tooltip />
+                </BarChart>
+              </ResponsiveContainer>
+            )}
           </Paper>
         </Paper>
       </div>
-      
     );
   }
+  
 }
 
 export default App;
